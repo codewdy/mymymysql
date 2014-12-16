5d7df9df1fcb3cde0767caebb8528af78af1ff90
Wang Dongyue <codewdy@gmail.com>
2014-12-16 19:31:50 +0800

    change BTree test

--- test/testBTree.cpp
+++ test/testBTree.cpp
@@ -6,6 +6,11 @@ int main() {
     PageDB::Scheduler* pgdb = new PageDB::LRU_Scheduler;
     pgdb->StartSchedule();
     BTree::BTree bt(pgdb, "testBTree.txt");
+    bt.set("test", BTree::Value(0, 0), true);
+    bt.remove("test");
+    auto X = bt.find("test");
+    if (X.first)
+        std::cout << "ERROR" << std::endl;
     for (int i = 0; i < 10000; i++)
         bt.set(std::to_string(i), BTree::Value(i * 2, i), true);
     for (int i = 0; i < 10000; i++) {
