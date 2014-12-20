fb064e8ef6a1b2f252be15315cf6cdbd6ba001b5
Wang Dongyue <codewdy@gmail.com>
2014-12-20 21:40:05 +0800

    Select From Where Done

--- Stmt/selectStmt.cpp
+++ Stmt/selectStmt.cpp
@@ -5,9 +5,21 @@ namespace Stmt {
     void SelectStmt::Run(Context::Context& ctx) {
         auto tbl = from[0]->getTable(ctx);
         if (selectAll) {
-            for (auto row : tbl.rows) {
-                for (auto item : row.objs)
+            for (auto& row : tbl.rows) {
+                for (auto& item : row.objs)
                     std::cout << item->toString() << " ";
+                std::cout << std::endl;
+            }
+        } else {
+            std::vector<std::vector<TypeDB::pObject>> objects;
+            for (auto& item : select)
+                objects.push_back(item->Calc(tbl));
+            std::size_t size = 0xFFFFFFFF;
+            for (auto& item : objects)
+                size = size < item.size() ? size : item.size();
+            for (std::size_t i = 0; i < size; i++) {
+                for (auto& item : objects)
+                    std::cout << item[i]->toString() << " ";
                 std::cout << std::endl;
             }
         }
