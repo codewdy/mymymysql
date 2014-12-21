df6a7e914cb17224f93cde145721e47eddf7d54c
Wang Dongyue <codewdy@gmail.com>
2014-12-22 07:34:24 +0800

    implement the filter for join

--- Stmt/filter.cpp
+++ Stmt/filter.cpp
@@ -2,8 +2,33 @@
 
 namespace Stmt {
     TypeDB::Table filter(const Context::Context& ctx, const std::vector<TypeDB::TableSelector*>& tbls, TypeDB::Expr* cond, bool force) {
-        //TODO
-        return filter(ctx, tbls[0], cond);
+        auto ret = filter(ctx, tbls[0], cond, true);
+        for (std::size_t i = 1; i < tbls.size(); i++) {
+            auto new_tbl = filter(ctx, tbls[i], cond, true);
+            auto desc = TypeDB::TableDesc::merge(ret.desc, new_tbl.desc);
+            ret.desc = desc;
+            std::vector<TypeDB::Row> rows;
+            for (auto& row1 : ret.rows)
+                for (auto& row2 : new_tbl.rows) {
+                    auto row = TypeDB::Row::merge(row1, row2);
+                    if (cond) {
+                        auto x = cond->CalcBool(desc, row);
+                        if (!x.first || x.second)
+                            rows.push_back(row);
+                    } else {
+                        rows.push_back(row);
+                    }
+                }
+            ret.rows.swap(rows);
+        }
+        if (cond && !force) {
+            for (auto& row : ret.rows)
+                if (!cond->CalcBool(ret.desc, row).first) {
+                    //TODO
+                    throw "Not Imp";
+                }
+        }
+        return std::move(ret);
     }
     TypeDB::Table filter(const Context::Context& ctx, TypeDB::TableSelector* tbl, TypeDB::Expr* cond, bool force) {
         TypeDB::Table ret = tbl->getTable(ctx);
@@ -11,16 +36,16 @@ namespace Stmt {
             std::vector<TypeDB::Row> rows;
             for (auto& row : ret.rows) {
                 auto x = cond->CalcBool(ret.desc, row);
-                if (force && x.first) {
+                if (force && !x.first) {
                     //TODO
                     throw "Not Imp";
                 }
-                if (x.first || x.second)
+                if (!x.first || x.second)
                     rows.push_back(std::move(row));
             }
             ret.rows.swap(rows);
         }
-        return ret;
+        return std::move(ret);
     }
     TypeDB::Table filter(const Context::Context& ctx, const std::string& tblName, TypeDB::Expr* cond, bool force) {
         TypeDB::Table ret = ctx.GetTable(tblName);
@@ -28,15 +53,15 @@ namespace Stmt {
             std::vector<TypeDB::Row> rows;
             for (auto& row : ret.rows) {
                 auto x = cond->CalcBool(ret.desc, row);
-                if (force && x.first) {
+                if (force && !x.first) {
                     //TODO
                     throw "Not Imp";
                 }
-                if (x.first || x.second)
+                if (!x.first || x.second)
                     rows.push_back(std::move(row));
             }
             ret.rows.swap(rows);
         }
-        return ret;
+        return std::move(ret);
     }
 }
--- TypeDB/table.cpp
+++ TypeDB/table.cpp
@@ -2,6 +2,13 @@
 #include "Exception/Exception.hpp"
 
 namespace TypeDB {
+    Row Row::merge(const Row& lhs, const Row& rhs) {
+        Row ret;
+        ret.objs.reserve(lhs.objs.size() + rhs.objs.size());
+        ret.objs.insert(ret.objs.end(), lhs.objs.begin(), lhs.objs.end());
+        ret.objs.insert(ret.objs.end(), rhs.objs.begin(), rhs.objs.end());
+        return std::move(ret);
+    }
     int TableDesc::getIndex(const std::string& tbl, const std::string& name, bool force) const {
         int result = -1;
         if (tbl == "") {
@@ -49,6 +56,14 @@ namespace TypeDB {
     }
     pObject TableDesc::getPrimary(const Row& row) const {
         return row.objs[primaryIndex];
+    }
+    TableDesc TableDesc::merge(const TableDesc& lhs, const TableDesc& rhs) {
+        TableDesc ret;
+        ret.primaryIndex = lhs.primaryIndex;
+        ret.descs.reserve(lhs.descs.size() + rhs.descs.size());
+        ret.descs.insert(ret.descs.end(), lhs.descs.begin(), lhs.descs.end());
+        ret.descs.insert(ret.descs.end(), rhs.descs.begin(), rhs.descs.end());
+        return std::move(ret);
     }
     std::vector<pObject> Table::getVec(const std::string& tbl, const std::string& name) const {
         std::size_t index = desc.getIndex(tbl, name);
